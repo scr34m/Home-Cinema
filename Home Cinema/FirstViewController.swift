7ea5ebd96ec2e93067f053a7fefc816b10428bfc
Gabor Gyorvari <scr34m@gmail.com>
2015-11-13 16:08:55 +0100

    Initial Commit

---
+++ Home Cinema/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Home Cinema
+//
+//  Created by Gyorvari Gabor on 2015. 11. 13..
+//  Copyright © 2015. Gyorvari Gabor. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Home Cinema/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Home Cinema
+//
+//  Created by Gyorvari Gabor on 2015. 11. 13..
+//  Copyright © 2015. Gyorvari Gabor. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
